b8e400bdadb088162b736f162f2a5beba9e4232d
gebre-tech <gebremeskelshimels1@gmail.com>
2025-05-22 07:22:19 +0300

    production for files1

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,7 +1,7 @@
 // Constants for the application
 // These constants are used throughout the application for various purposes
-export const API_HOST = "192.168.137.1:8000";
-export const API_URL = `http://${API_HOST}`; // Adjust this to your backend URL
+export const API_HOST = "message-app-2025.onrender.com";
+export const API_URL = `https://${API_HOST}`; // Adjust this to your backend URL
 
 // Icon names instead of placeholder URLs
 export const PLACEHOLDER_IMAGE_ICON = 'image'; // Default icon name for images
